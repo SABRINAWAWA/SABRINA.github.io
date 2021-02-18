c8b462401d2a0a28e6b039b5866ebfa2bf8a1011
Huiying-Sabrina <53490593+SABRINAWAWA@users.noreply.github.com>
2021-02-17 23:00:16 -0800

    Java 111B Assignment 13
    Java 111B Introduction to Programming of JAVA

--- MyCircle.java
+++ MyCircle.java
@@ -1,92 +1,22 @@
-import java.text.DecimalFormat;
 
-public class MyCircle 
-{
-	private double radius;
-	private double x;
-	private double y;
-	
-	/**
-	 * The setRadius method stores a value in the radius field.
-	 * @param rad The value to store in radius. 
-	 */
-	public void setRadius(double rad)
-	{
-		radius=rad;
+public class MyCircle extends GridItem {
+
+	private int radius;
+
+	public MyCircle(int xValue, int yValue, int r) {
+		x = xValue;
+		y = yValue;
+		radius = r;
 	}
-	
-	/**
-	 * The setX method stores a value in the x field.
-	 * @param xaxis The value to store in x;
-	 */
-	public void setX(double xaxis)
-	{
-		x=xaxis;
+
+	public double getArea() {
+		return Math.PI * Math.pow(radius, 2);
 	}
-	
-	/**
-	 * The setY method stores a value in the y field.
-	 * @param yaxis The value to store in y;
-	 */
-	public void setY(double yaxis)
-	{
-		y=yaxis;
-	}
-	
-	/**
-	 * The getRadius method returns a MyCircle object's radius.
-	 * @return The value in the radius field.
-	 */
-	public double getRadius()
-	{
-		return radius;
-	}
-	
-	/**
-	 * The getX method returns a MyCircle object's x.
-	 * @return The value in the x field.
-	 */
-	public double getX()
-	{
-		return x;
-	}
-	
-	/**
-	 * The getY method returns a MyCircle object's y.
-	 * @return The value in the y field.
-	 */
-	public double getY()
-	{
-		return y;
-	}
-	
-	/**
-	 * The getArea method returns a MyCircle object's area. 
-	 * @return The product of radius times 3.14159.
-	 */
-	public double getArea()
-	{
-		double area=Math.PI*radius*radius;
-		area=Double.parseDouble(new DecimalFormat("##.##").format(area));
-		return area;
-	}
-	/**
-	 * The doesOverlap method returns true if two circle overlap, returns false if two circle does 
-	 * not overlap.
-	 * @param otherCircle Another Circle that contains x, y, and radius. 
-	 * @return true if two circle overlap, false if two circle does not overlap. 
-	 */
-	public boolean doesOverlap(MyCircle otherCircle)
-	{
-		double sum_of_radius=radius+otherCircle.radius;
-	    double distance = Math.sqrt((x - otherCircle.x) * (x - otherCircle.x) + (y - otherCircle.y) * (y - otherCircle.y));
-	    if (sum_of_radius >= distance) 
-	    {
-	    	System.out.print("This circle is overlapping with another circle.\n");
-	        return true;
-	    } else {
-	    	System.out.print("This circle does not overlap with another circle.\n");
-	        return false;
-	    }
+
+	public boolean containsPoint(int xValue, int yValue) {
+		double dx = x - xValue;
+		double dy = y - yValue;
+		double distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+		return distance <= radius;
 	}
 }
